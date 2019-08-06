1e8ace95679870b68dd75b4f7011cae7f25ad1f8
rockn1989 <gold_100@bk.ru>
2019-08-06 19:05:22 +0300

    Переписал компонент Jsx

--- app.jsx
+++ app.jsx
@@ -1,4 +1,4 @@
-var jsx = React.createClass({
+/* var jsx = React.createClass({
 	render: function () {
 		return(
 			<div className="container">
@@ -11,4 +11,16 @@ var jsx = React.createClass({
 });
 
 
-ReactDOM.render(jsx, document.getElementById('container'));
+ReactDOM.render(jsx, document.getElementById('container')); */
+
+function Jsx(props) {
+	return (
+		<div className="container">
+			<img src="https://via.placeholder.com/400x400" />
+			<h1>React</h1>
+			<p>Первый запуск реакт</p>
+		</div>
+	)
+}
+
+ReactDOM.render(<Jsx />, document.getElementById('container'));
